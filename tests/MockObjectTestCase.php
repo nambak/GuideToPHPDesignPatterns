6074f8e7a307fb566080c477e71e332dcfdfe80d
nambak80 <nambak80@gmail.com>
2021-07-14 23:09:24 +0900

    add example of chapter 6

--- tests/MockObjectTestCase.php
+++ tests/MockObjectTestCase.php
@@ -13,5 +13,24 @@ final class MockObjectTestCase extends TestCase
         $this->assertEquals(6, $sum->total());
     }
 
-    public function testCalc
+    public function testCalcTax()
+    {
+        $amount = $this->createMock(Accumulator::class);
+        $amount->setReturnValue('total', 200);
+        $amount->expectOnce('total');
+
+        $this->assertEquals(14,  cal_tax($amount));
+
+        $amount->tally();
+    }
+
+    public function testCalcTotalAgain()
+    {
+        $sum = $this->createMock(Accumulator::class);
+        $sum->expectOnce('add');
+
+        calc_total([1, 2, 3], $sum);
+
+        $sum->tally();
+    }
 }
